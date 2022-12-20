03e1229ddb3fa0a07055759734861e1b7f3c9eeb
Fırat Altun <frtaltn.psy@gmail.com>
2022-12-21 00:53:39 +0300

    Searched: sayfa oluşturma, api ve style ayarlama.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
 
   const submitHandler = (e) => {
     e.preventDefault();
+    navigate("/searched/" + input);
   };
 
   return (
