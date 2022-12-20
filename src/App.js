3b5ce95ae00cbd6d9bacee0e74a72397ab89ec62
Fırat Altun <frtaltn.psy@gmail.com>
2022-12-21 02:57:53 +0300

    Logo ve animasyon yapılandırldı. proje tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>delicious</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,4 +21,23 @@ function App() {
   );
 }
 
+// Style settings
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`;
 export default App;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Searched() {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -24,8 +25,10 @@ function Searched() {
       {searchedRecipes.map((item) => {
         return (
           <Card key={item.id}>
-            <img src={item.image} alt="" />
-            <h4>{item.title}</h4>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Link>
           </Card>
         );
       })}
