a69059f35a73f4227b0689ad838054f5680bd59f
Fırat Altun <frtaltn.psy@gmail.com>
2022-12-20 15:17:02 +0300

    Category: Kategori komponentinin oluşturulması

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
+      <Category />
       <Pages />
     </div>
   );
---
+++ src/components/Category.jsx
@@ -0,0 +1,36 @@
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import styled from "styled-components";
+
+function Category() {
+  return (
+    <List>
+      <div>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </div>
+      <div>
+        <FaHamburger />
+        <h4>American</h4>
+      </div>
+      <div>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </div>
+      <div>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </div>
+    </List>
+  );
+}
+
+// Style settings
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+
+export default Category;
